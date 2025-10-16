d0bc3bdcd4ebe5ee6f21ba5830d6d9070b480a26
Jcperd32 <150964794+Jcperd32@users.noreply.github.com>
2025-10-16 00:22:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,17 +17,25 @@ function typeWriter(text, elementId, speed = 50) {
 
 window.onload = function() {
     const riddleText = `Welcome to this project. This is a test style page, but don't worry you don't have to study. It is more about how you are feeling.
+    
 
         It might seem a bit conceited on my part for thinking anything I write is worth reading, but I wasn't sure how to get any feedback honestly, and I was unsure if I was going to share this anyway. But here it is.
         
+        
         Enter your name, just your first name. Whether I know you or not is fine, and make sure to leave some feedback at the end.
+        
         
         Hopefully this riddle keeps you thinking for the rest of the time you are here:
         
+        
         I'm a cache with no processor,
+        
         A library with no shelves.
+        
         I'm recalled but not forgotten,
+        
         And fragmented over time.
+        
         What am I?`;
     typeWriter(riddleText, 'riddle-text', 60); 
 }
